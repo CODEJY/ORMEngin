cf1ca3faa3dcc498ab2e4b367285153d00d61f6d
CODEJY <wujy1284@gmail.com>
2017-12-28 04:32:55 +0800

    fix bug

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var user2 entity.UserInfo
 var engine *dao.ORMEngine
 
 func init() {
-	engine = dao.NewEngine("mysql", "root:root@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=true")
+	engine = dao.NewEngine("mysql", "root:331284@tcp(127.0.0.1:3306)/UserDB?charset=utf8&parseTime=true")
 	t := time.Now()
 	user1 = entity.UserInfo{
 		UserName:   "wujy",
@@ -29,23 +29,23 @@ func init() {
 	}
 }
 func main() {
-	result, err := engine.Insert(user1)
+	_, err := engine.Insert(user1)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%d row(s) result after inserting %s\n", result, user1.UserName)
-	result, err = engine.Insert(user2)
+	fmt.Printf("User: %s insert successfully!\n", user1.UserName)
+	_, err = engine.Insert(user2)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%d row(s) result after inserting %s\n", result, user2.UserName)
+	fmt.Printf("User: %s insert successfully!\n", user2.UserName)
 
 	allUsers := make([]*entity.UserInfo, 0)
 	err = engine.Find(&allUsers)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("all users: ")
+	fmt.Println("Find all users: ")
 	for i := 0; i < len(allUsers); i++ {
 		fmt.Println(*allUsers[i])
 	}
